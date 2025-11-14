43c249d37d5a42743063ef2723d3485085ef387d
Giorgi Kostava <gkostava03@gmail.com>
2025-11-14 10:57:31 +0400

    update script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+let num1 = 20;
+
+if (num1 * num1 > 100) {
+  console.log("მეტია 100-ზე");
+} else {
+  console.log("ნაკლებია 100-ზე");
+}
+
+let num2 = 25;
+
+if (num2 > 50 && num2 < 100) {
+  console.log("რიცხვი არის 50-დან 100-მდე");
+} else {
+  console.log("რიცხვი არ არის 50-დან 100-მდე");
+}
+
+let num3 = -10;
+
+if (num3 >= 0) {
+  console.log("დადებითია");
+} else {
+  console.log("უარყოფითია");
+}
+
+let num4 = 10;
+let num5 = 15;
+
+if ((num4 + num5) * 2 > 50) {
+  console.log("პერიმეტრი მეტია 50-ზე");
+} else {
+  console.log("პერიმეტრი ნაკლებია 50-ზე");
+}
